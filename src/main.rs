3438f86239c9816ff7201923007f79cd1ed34b51
Morgan Mulholland <morgan.p.mulholland@gmail.com>
2024-01-21 18:17:55 +0000

    started defining noisemaps 2d and 3d and implementing some functions for them

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
-
+mod noise;
+use noise::*;
 fn main() {
     
     let _ = App::new()
--- src/noise.rs
+++ src/noise.rs
@@ -2,36 +2,20 @@
 use std::clone;
 
 use libnoise::prelude::*;
-use bevy::prelude::*;
-use rand::prelude::*;
+use bevy::{prelude::*, math::f32};
 
 
 
-use super::basic::*;
 
 #[derive(Clone)]
 pub struct NoiseValues
 {
     pub octaves: usize,
-    pub scale: f64,
-    pub persistence: f64,
-    pub lacunarity: f64
+    pub scale: f32,
+    pub persistence: f32,
+    pub lacunarity: f32
 }
 
-#[derive(Resource, Clone)]
-pub struct TerrainNoiseGenerator
-{
-    pub biome_generator: NoiseGenerator,
-    pub height_generator: NoiseGenerator
-}
-impl TerrainNoiseGenerator
-{
-    pub fn update(&mut self, custom_biome_generator: NoiseGenerator, custom_height_generator: NoiseGenerator)
-    {                
-        self.biome_generator = custom_biome_generator;
-        self.height_generator = custom_height_generator;
-    }    
-}
 
 
 #[derive(Resource, Clone)]
@@ -46,14 +30,8 @@ pub struct NoiseGenerator
 }
 impl NoiseGenerator {    
   
-    pub fn get_combined_value_at(&self, x: i32, y: i32) -> f64
-    {
-        let perlin = self.get_perlin_value_at(x, y);
-        let simplex = self.get_simplex_value_at(x, y);
-        let worley = self.get_worley_value_at(x, y);
-        return (perlin + simplex + worley) / 3.0;
-    }
-    pub fn get_combined_value_at_weighted(&self, x: i32, y: i32, perlin_weight: f64, simplex_weight: f64, worley_weight: f64) -> f64
+    
+    pub fn get_combined_value_at_weighted(&self, x: i32, y: i32, perlin_weight: f32, simplex_weight: f32, worley_weight: f32) -> f32
     {
         let perlin = self.get_perlin_value_at(x, y) * perlin_weight;
         let simplex = self.get_simplex_value_at(x, y) * simplex_weight;
@@ -62,87 +40,106 @@ impl NoiseGenerator {
         return perlin * ratio + simplex * ratio + worley * ratio;
     }
 
-    pub fn get_perlin_value_at(&self, x: i32, y: i32) -> f64 {
+
+    pub fn get_perlin_value_at(&self, x: i32, y: i32) -> f32 {
 
         let mut sum = 0.0;
-        let mut amplitude = 1.0;
+        let mut amplitude: f32 = 1.0;
         let mut frequency = self.perlin_noise_values.scale;
-        let mut max_amplitude = 1.0;      
-
-     
-        
-
-
+        l
         for _ in 0..self.perlin_noise_values.octaves {
-            let new_x: f64 = x as f64 * frequency;
-            let new_y: f64 = y as f64 * frequency;
-            let sample: f64 = self.perlin_source.sample([new_x, new_y]);
-            sum += sample * amplitude;
-
-            max_amplitude += amplitude;
+            let new_x: f32 = x as f32 * frequency;
+            let new_y: f32 = y as f32 * frequency;
+            let sample: f64 = self.perlin_source.sample([new_x as f64, new_y as f64]);
+            sum += sample as f32 * amplitude;
+            
             amplitude *= self.perlin_noise_values.persistence;
             frequency *= self.perlin_noise_values.lacunarity;
         }
-
        
         return sum;
     }
 
-    pub fn get_simplex_value_at(&self, x: i32, y: i32) -> f64 {
 
-        let mut sum = 0.0;
-        let mut amplitude = 1.0;
-        let mut frequency = self.simplex_noise_values.scale;
-        let mut max_amplitude = 1.0;      
+    pub fn get_simplex_value_at(&self, x: i32, y: i32) -> f32 {
 
-        
-        
-
+        let mut sum: f32 = 0.0;
+        let mut amplitude: f32 = 1.0;
+        let mut frequency: f32 = self.simplex_noise_values.scale;
+        let mut max_amplitude = 1.0;                                        
 
         for _ in 0..self.simplex_noise_values.octaves {
-            let new_x: f64 = x as f64 * frequency;
-            let new_y: f64 = y as f64 * frequency;
-            let sample: f64 = self.simplex_source.sample([new_x, new_y]);
+            let new_x: f32 = x as f32 * frequency;
+            let new_y: f32 = y as f32 * frequency;
+            let sample: f32 = self.simplex_source.sample([new_x as f64, new_y as f64]) as f32;
             sum += sample * amplitude;
 
             max_amplitude += amplitude;
             amplitude *= self.simplex_noise_values.persistence;
             frequency *= self.simplex_noise_values.lacunarity;
         }
-
        
-        return sum;
+        return sum as f32;
     }
 
-    pub fn get_worley_value_at(&self, x: i32, y: i32) -> f64 {
 
-        let mut sum = 0.0;
-        let mut amplitude = 1.0;
-        let mut frequency = self.worley_noise_values.scale;
-        let mut max_amplitude = 1.0;      
+    pub fn get_worley_value_at(&self, x: i32, y: i32) -> f32 {
 
-        
-        
-
+        let mut sum: f32 = 0.0;
+        let mut amplitude: f32 = 1.0;
+        let mut frequency: f32  = self.worley_noise_values.scale;
+                                                     
 
         for _ in 0..self.worley_noise_values.octaves {
-            let new_x: f64 = x as f64 * frequency;
-            let new_y: f64 = y as f64 * frequency;
-            let sample: f64 = self.worley_source.sample([new_x, new_y]);
+            let new_x: f32 = x as f32 * frequency;
+            let new_y: f32 = y as f32 * frequency;
+            let sample: f32 = self.worley_source.sample([new_x as f64, new_y as f64]) as f32;
             sum += sample * amplitude;
 
-            max_amplitude += amplitude;
+            
             amplitude *= self.worley_noise_values.persistence;
             frequency *= self.worley_noise_values.lacunarity;
         }
-
        
-        return -sum;
+        return sum as f32;
     }
 
 }
 
 
+pub struct NoiseMap2D
+{
+    pub width: usize,
+    pub height: usize,
+    pub buffer: Vec<f32>
+} 
+impl NoiseMap3D
+{   
+    fn initialize(&self)
+    {
+        self.buffer = Vec::with_capacity(self.width * self.height)
+        
+    }
+    fn get_value_at(x: u32, y: u32)
+    {
 
+    }
+    fn set_value_at(x: u32, y: u32)
+    {
+
+    }
+}
+
+pub struct NoiseMap3D
+{
+    pub width: u32,
+    pub height: u32,
+    pub depth: u32,
+    pub buffer: Vec<f32>
+}
+impl NoiseMap3D
+{
+
+}
 
 
